f5b351d714537d24f4ab25e0283de12afb71ca58
liv3 <liv3@localhost>
2025-06-23 10:15:57 +0530

    -

---
+++ GuviTask/src/JavaPackage/GuviTasks5_Q1.java
@@ -0,0 +1,37 @@
+//Palindrome
+
+package JavaPackage;
+
+import java.util.Scanner;
+
+public class GuviTasks5_Q1 {
+
+	public static void main(String[] args) {
+
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str = obj.next();
+
+		int i, j = 0;
+		boolean isPal = true;
+
+		for(i = str.length() - 1; i > j; i--) {
+			
+			if (str.charAt(i) == str.charAt(j)){
+				isPal = true;
+			} else {
+				isPal = false;
+			}
+			
+			j++;
+		}
+
+		if (isPal == true) {
+			System.out.println("Palindrome");
+		} else {
+			System.out.println("Not Palindrome");
+		}
+
+	}
+
+}
