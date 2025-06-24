57b686999890053be91ca0dc7f17d94bfb235396
liv3 <liv3@localhost>
2025-06-24 11:31:24 +0530

    -

--- GuviTask/src/task6/Q1_mainclass.java
+++ GuviTask/src/task6/Q1_mainclass.java
@@ -2,7 +2,7 @@ package task6;
 
 public class Q1_mainclass {
 	    public static void main(String[] args) {
-	        Person p = new Person("Alice", 25);
+	        Person p = new Person("Prem", 25);
 	        System.out.println("Name: " + p.getName());
 	        System.out.println("Age: " + p.getAge());
 	    }
