04b7a98b692aef44ed82b94aaf89e30ac913d8d9
liv3 <liv3@localhost>
2025-06-24 20:51:20 +0530

    -

--- GuviTask/src/task6/Q2_mainclass.java
+++ GuviTask/src/task6/Q2_mainclass.java
@@ -3,11 +3,11 @@ package task6;
 public class Q2_mainclass {
 
 	public static void main(String[] args) {
-		Employee emp = new Employee(101, "Prem", "Vishnu", 10000);
-        System.out.println(emp); 
-        emp.raiseSalary(10); 
-        System.out.println("New salary: " + emp.getSalary());
-        System.out.println("Annual salary: " + emp.getAnnualSalary());
+		Employee obj = new Employee(101, "Prem", "Vishnu", 10000);
+        System.out.println(obj); 
+        obj.raiseSalary(10); 
+        System.out.println("New salary: " + obj.getSalary());
+        System.out.println("Annual salary: " + obj.getAnnualSalary());
 
 	}
 
